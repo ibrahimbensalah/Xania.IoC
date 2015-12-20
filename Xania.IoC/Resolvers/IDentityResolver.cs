b16491591c281ca64af829b6dd56ad81db2adf74
ibrahimbensalah <ibrahim.bensalah@gmail.com>
2015-12-20 16:01:50 +0100

    identity resolver

---
+++ Xania.IoC/Resolvers/IDentityResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xania.IoC.Resolvers
+{
+    public class IdentityResolver: IResolver
+    {
+        public ICollection<Func<Type, bool>> Predicates { get; private set; }
+
+        public IdentityResolver()
+        {
+            Predicates = new List<Func<Type, bool>>();
+        }
+
+        public void Include(Func<Type, bool> predicate)
+        {
+            Predicates.Add(predicate);
+        }
+
+        public IResolvable Resolve(Type type)
+        {
+            return Predicates.Any(p => p(type)) ? TypeResolvable.Create(type) : null;
+        }
+    }
+}
